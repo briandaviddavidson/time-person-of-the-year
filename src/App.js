184e7c686571f0d027320f8cd2360d923814b03d
Brian Davidson <briandaviddavidson@gmail.com>
2020-04-18 21:03:39 -0400

    Restructure jsx and build search url for name

--- src/App.js
+++ src/App.js
@@ -4,16 +4,31 @@ import './App.css';
 let people = require('./data/people.json');
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.buildUrl = this.buildUrl.bind(this);
+  }
+
+  buildUrl(name) {
+    let nameArr = name.split('(');
+    let val = nameArr.length > 1 ? nameArr[1].slice(0, -1) : nameArr[0] ;
+    let search = val.replace(' ', '_');
+    return `https://en.wikipedia.org/wiki/${search}`;
+  }
   render() {
-    return (<div class="flex">
+    return (<div className="">
     <img src={logo} alt="Time Magazine" />
-      <div className="users">
+      <div className="">
         {
-          people.map((user, index) => (<div key={index}>
-            <h3>{user.name}</h3>
-            <p>{user.year}</p>
-            <p>{user.birth || "Various"}</p>
-          </div>))
+          people.map((user, index) => (
+            <div key={index}>
+              <h3><a href={this.buildUrl(user.name)}>{user.name}</a></h3>
+              <p>{user.country || "Various"}</p>
+              <p>Title: {user.title}</p>
+              <p>Category: {user.category}</p>
+              <p>Context: {user.context}</p>
+            </div>
+          ))
         }
       </div>
     </div>)
