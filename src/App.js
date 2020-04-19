bfc5fb62bbd1a4e5485e90d8991ff25cf6d66b65
Brian Davidson <briandaviddavidson@gmail.com>
2020-04-18 21:04:00 -0400

    Add birth/death & age

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ class App extends React.Component {
   constructor() {
     super();
     this.buildUrl = this.buildUrl.bind(this);
+    this.setBirthAndDeath = this.setBirthAndDeath.bind(this);
+    this.calcAge = this.calcAge.bind(this);
   }
 
   buildUrl(name) {
@@ -15,6 +17,25 @@ class App extends React.Component {
     let search = val.replace(' ', '_');
     return `https://en.wikipedia.org/wiki/${search}`;
   }
+
+  setBirthAndDeath(birth, death) {
+    if (!birth && !death) {
+      return ''
+    } else if (birth && !death) {
+      return `Born in ${birth}`;
+    } else {
+      return `${birth}-${death}`;
+    }
+  }
+
+  calcAge(birth, year) {
+    if (!birth) {
+      return '';
+    }
+    let age = year-birth;
+    return `at the age of ${age}`;
+  }
+
   render() {
     return (<div className="">
     <img src={logo} alt="Time Magazine" />
@@ -23,7 +44,9 @@ class App extends React.Component {
           people.map((user, index) => (
             <div key={index}>
               <h3><a href={this.buildUrl(user.name)}>{user.name}</a></h3>
+              <p>{user.honor} in {user.year} {this.calcAge(user.birth, user.year)}</p>
               <p>{user.country || "Various"}</p>
+              <p>{this.setBirthAndDeath(user.birth, user.death)}</p>
               <p>Title: {user.title}</p>
               <p>Category: {user.category}</p>
               <p>Context: {user.context}</p>
