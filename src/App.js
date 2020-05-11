b101deb669a293cd68226c559725dc33ec702d55
Brian Davidson <briandaviddavidson@gmail.com>
2020-05-11 12:02:26 -0400

    Update styling

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ let people = require('./data/people.json');
 class App extends React.Component {
   render() {
     return (
-      <div className="">
-      <img src={logo} alt="Time Magazine" />
-        <Table people={people} />
+      <div>
+        <div className="logo">
+          <span className="extra">TIME PERSON OF THE YEAR </span>
+        </div>
+        <Table people={people} className="table-flex"/>
       </div>
     )
   }
